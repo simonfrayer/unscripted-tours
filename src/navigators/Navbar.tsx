28aa2f3b0ff1a382c280bc48d0bbaa20a7758ed7
simonmayer <simon.mayer@rigi.tech>
2023-10-01 10:38:32 +0200

    navbar simplified

--- src/navigators/Navbar.tsx
+++ src/navigators/Navbar.tsx
@@ -1,120 +1,25 @@
-import React from "react";
-import { AppBar, IconButton, Toolbar, Typography, Container, Box, Menu, MenuItem, Button } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import AdbIcon from '@mui/icons-material/Adb';
-import LogoSvg from "../assets/Unscripted.svg";
+import { AppBar, Toolbar, Container, Box, Button } from "@mui/material";
 
 const pages = ['How does it work', 'Cooperation', 'About us'];
 
 export const Navbar = () => {
-    
-const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-
-const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
     return (
-            <AppBar position='static'>
-                 <Container maxWidth="xl">
-        <Toolbar disableGutters>
-
-          {/* small screen (tablet -) */}
-        <img src={LogoSvg} width="60vw" height={"auto"} alt="Logo"/>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'end', }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'block', md: 'none' },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
+      <AppBar position='static'>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+        {/* <img src={LogoSvg} width="60vw" height={"auto"} alt="Logo"/> */}
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' }, justifyContent: 'center' }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
             </Box>
-
-          {/* large screen (laptop +)*/}
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              alignText: 'end',
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-
-    
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'end' }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-            </AppBar>
+          </Toolbar>
+        </Container>
+      </AppBar>
     )
 }
