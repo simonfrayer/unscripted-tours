9374fe6299af5562f9c52fda936192e2165da5e4
simonmayer <simon.mayer@rigi.tech>
2023-10-03 20:41:18 +0200

    created container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,28 @@
-const Container = () => {
+import { Grid, Typography } from "@mui/material";
+import { FC, ReactNode } from "react";
+
+interface ContainerProps {
+    title? : string,
+    backgroundColor? : string,
+    children: ReactNode
+}
+
+const Container: FC<ContainerProps> = ({title, backgroundColor, children}) => {
     return (
         <>
+            <Grid container spacing={1} sx={{backgroundColor: backgroundColor}}>
+                {title? 
+                    <Grid item xs={12}>
+                        <Typography variant="h5" align="center">
+                            {title}
+                        </Typography>
+                    </Grid>
+                : null}
+
+                <Grid item xs={12}>
+                    {children}
+                </Grid>
+            </Grid>
         </>
     )
 }
