254528010b9479b13a8ecbb6d828ea911d8d0811
simonmayer <simon.mayer@rigi.tech>
2023-10-07 22:22:19 +0200

    fixed box styling

--- src/tours/DolinaApartment.tsx
+++ src/tours/DolinaApartment.tsx
@@ -12,8 +12,12 @@ const DolinaApartment: FC<DolinaApartmentProps> = () => {
             width: "80%", 
             height: "auto",
             boxShadow: 3,
+            borderRadius: 10,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
             }}>
-            <iframe width="90%" height="100%" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7JQY7?logo=1&info=1&fs=1&vr=0&sd=1&autorotate=0.08&thumbs=3" style={{color: "white"}}></iframe>        </Box>
+            <iframe width="90%" height="90%" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7JQY7?logo=1&info=1&fs=1&vr=0&sd=1&autorotate=0.08&thumbs=3" style={{color: "white"}}></iframe>        </Box>
         </>
     )
 }
