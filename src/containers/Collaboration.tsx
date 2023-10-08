0b380c4b019faf2c81c0fb8f75821c524da949ed
simonmayer <simon.mayer@rigi.tech>
2023-10-08 20:53:43 +0200

    small work on the form

--- src/containers/Collaboration.tsx
+++ src/containers/Collaboration.tsx
@@ -1,25 +1,51 @@
 import Container from "../components/Container";
 import ContainerBackgroundImage from "../assets/container-2.jpg";
 import BackgroundImage from "../assets/contact-form.jpeg";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, FormControl, Input, InputLabel,FormHelperText } from "@mui/material";
+import theme from "../utils/theme";
 
 const Collaboration = () => {
     return (
         <>
             <Container title="Collaboration" backgorundImage={ContainerBackgroundImage} titlePosition="start">
-                <Grid item xs={12}>
-                    <Typography variant="body1">
+                <Grid item xs={12} sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Typography variant="body1" sx={{width: "60%"}}>
                         Collaborating with a 3D Virtual Tour business ensures a competitive edge by offering an immersive, visually engaging experience that transcends geographical limitations, enhances customer confidence, and provides a cost-effective, versatile solution across diverse industries.
                     </Typography>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={12} sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                     <Box sx={{
-                        width: "100%",
+                        width: "60%",
+                        heigth: "",
                         backgroundImage: `url(${BackgroundImage})`,
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
+                        padding: 5,
+                        display: "flex", 
+                        flexDirection: "column",
+                        justifyContent: "center", 
+                        alignItems: "center",
                     }}>
-
+                        <Typography variant="h3" sx={{padding: 2, paddingTop: 0}}>
+                            Contact Us
+                        </Typography>
+                        <Box sx={{
+                            padding: 2, 
+                            width: "80%", 
+                            height: "auto",
+                            boxShadow: 3,
+                            borderRadius: 10,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            backgroundColor: theme.palette.primary.main,
+                        }}>
+                            <FormControl>
+                                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                                <Input id="my-input" aria-describedby="my-helper-text" />
+                                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                            </FormControl>
+                        </Box>
                     </Box>
                 </Grid>
             </Container>
